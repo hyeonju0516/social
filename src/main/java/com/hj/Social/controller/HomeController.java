4a6a7a616eb853547630475a000a77c47eb60de0
DESKTOP-GD3SP7B\User <hana_smile@naver.com>
2024-01-09 17:02:19 +0900

    0109 초기세팅

---
+++ src/main/java/com/hj/Social/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.hj.Social.controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/home")
+	public void home(Locale locale, Model model) {
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		String formattedDate = dateFormat.format(date);
+		model.addAttribute("serverTime", formattedDate );
+	} //home
+	
+}
