375b7c1c2f3d78a121d94b98d6a2c77f8de9eea6
DESKTOP-GD3SP7B\User <hana_smile@naver.com>
2024-02-07 13:40:08 +0900

    0207 설정 변경

--- src/main/java/com/hj/Social/SocialApplication.java
+++ src/main/java/com/hj/Social/SocialApplication.java
@@ -3,14 +3,20 @@ package com.hj.Social;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication(exclude= {SecurityAutoConfiguration.class})
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EnableJpaAuditing
-public class SocialApplication {
+public class SocialApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SocialApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SocialApplication.class, args);
+    }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SocialApplication.class);
+    }
 }
