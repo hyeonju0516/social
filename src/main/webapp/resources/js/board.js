4b792c7c02c899804099a400a800ceb391ee3a5f
DESKTOP-GD3SP7B\User <hana_smile@naver.com>
2024-03-20 21:39:37 +0900

    0320 400에러 해결

--- src/main/webapp/resources/js/board.js
+++ src/main/webapp/resources/js/board.js
@@ -17,7 +17,7 @@ function boardDelete(board_id) {
 	let url = "/board/delete";
 
 	if (confirm("삭제하시겠습니까?")) {
-		axios.post(url, board_id,
+		axios.post(url, {board_id: board_id},
 			{ headers: { 'Content-Type': 'application/json' } }
 		).then(response => {
 
@@ -207,7 +207,7 @@ function commentDelete(comment_id) {
 
 
 	if (confirm("삭제하시겠습니까?")) {
-		axios.post(url, {comment_id},
+		axios.post(url, {comment_id:comment_id},
 			{ headers: { 'Content-Type': 'application/json' } }
 		).then(response => {
 			alert("삭제되었습니다.");
